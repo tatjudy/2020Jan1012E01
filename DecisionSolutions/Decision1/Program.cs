328dbab95d7dbae52c8cc45f7885d90174f3fd3f
tatjudy <59752406+tatjudy@users.noreply.github.com>
2020-01-22 18:49:08 -0700

    Creation of Decisions

---
+++ DecisionSolutions/Decision1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decision1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int first, second, highest = -99;
+            int first;          //Declare and set default to 0
+            int second;         //Declare and set default to 0
+            int highest = -99;  //Declared and set to -99
+            string message;     //Declared and set to null
+            string inputValue;
+
+            System.Console.Write("Enter first number:\t"); //Fully Qualified Method
+            //System.Console                               //Fully Qualified Class
+            inputValue = Console.ReadLine();
+            first = int.Parse(inputValue);
+
+            //Reuse inputValue because previous contents of input value has been stored elsewhere
+            System.Console.Write("Enter second number:\t");
+            inputValue = Console.ReadLine();
+            second = int.Parse(inputValue);
+
+            if (first > second)
+            {
+                //true path coding block
+                //the condition on the if statement is true
+                highest = first;
+                message = "first";
+            }
+
+            else
+            {
+                //false path coding block
+                //the condition on the if statement is false
+                highest = second;
+                message = "second";
+            }//eof
+            Console.WriteLine($"First = {first}, Second = {second}," +
+                $"the {message} number entered had the higher value: {highest}");
+            Console.ReadKey();
+        }//eom
+    }//eoc
+}//eon
---
+++ DecisionSolutions/EvenOrOdd/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int first, second, highest = -99;
+            int number;          //Declare and set default to 0
+            const int Two = 2;   //This is a non changable variable
+            string inputValue;
+
+            System.Console.Write("Enter a number:\t"); //Fully Qualified Method
+            //System.Console                               //Fully Qualified Class
+            inputValue = Console.ReadLine();
+            number = int.Parse(inputValue);
+
+            if ((number % Two) == 0)
+            {
+                //true path coding block
+                //the condition on the if statement is true
+                Console.WriteLine($"Your number {number} is even.");
+            }
+
+            else
+            {
+                //false path coding block
+                //the condition on the if statement is false
+                Console.WriteLine($"Your number {number} is odd.");
+            }//eof
+            Console.ReadKey();
+        } 
+    }
+}
