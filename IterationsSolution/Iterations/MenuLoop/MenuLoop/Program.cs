a94e49861e1367189d3429d485fa3a62cc21d669
tatjudy <59752406+tatjudy@users.noreply.github.com>
2020-02-12 18:44:27 -0700

    menuAndNestedLoop

---
+++ IterationsSolution/Iterations/MenuLoop/MenuLoop/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Generic menu setup
+            //loop: post-loop
+            //loop contents
+            string menuOption;
+            do
+            {
+                //Prompts
+                Console.WriteLine("Select an option from the following menu:");
+                Console.WriteLine("A) Do case A:");
+                Console.WriteLine("A) Do case B:");
+                Console.WriteLine("A) Do case C:");
+                Console.WriteLine("X) Exit\n");
+                Console.Write("Enter your menu option:\t");
+                menuOption = Console.ReadLine();
+
+                switch(menuOption.ToUpper())
+                {
+                    case "A":
+                        {
+                            //place any logic to do in case a
+                            Console.WriteLine($"You entered the menu option of A.\n\n");
+                            break;
+                        }
+
+                    case "B":
+                        {
+                            //place the heads or tails in this case
+                            //validate that an h or to was entered. if not, repeat query for input.
+                            Console.WriteLine($"You entered the menu option of B.\n\n");
+                            break;
+                        }
+
+                    case "C":
+                        {
+                            //the sum of squares
+                            //enter a positive integer number (validate)
+                            //loop n times where n is the integer number (1 to n)
+                            //within the loop your calculation is total += loopNumber + loopNumber
+                            //after the loop display your integer number and its sum of squares
+                            //entered 4
+                            //loop 4 times 1) 1 * 1 2) 2 * 2 3) 3 * 3 4) 4 * 4 final total = 30
+                            Console.WriteLine($"You entered the menu option of C.\n\n");
+                            break; 
+                        }
+
+                    case "X":
+                        {
+                            Console.WriteLine("Thank you. Come again.\n\n");
+                            break;
+                        }
+
+                    default:
+                        {
+                            //this case acts as a validation for the proper value of a menuOption.
+                            Console.WriteLine($"Your menu option choice of {menuOption} is incorrect. Try again.\n\n");
+                            break;
+                        }
+                }
+
+
+
+            } while (menuOption.ToUpper() != "X");
+            //as long as input isn't x, program will continue to run
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IterationsSolution/Iterations/MenuLoop/NestedLoop/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Query user for a number
+            //Validate a number was entered (integer)
+            //if a number was entered, check for even/odd
+            //Continue unless a 0 was entered, then exit
+
+
+            //to validate a string to contain a number
+            //  use the xxx.TryParse of the number datatype
+            //Syntax of .TryParse(string, out variableName)
+            //string hold the value to test
+            //if the value is good:
+            //          The number is automatically placed in your variable
+            //          The test returns a boolean true
+            //If the value is not good:
+            //          No value is placed in your variable
+            //          The test returns a boolean false
+
+            int number = -1;
+            string inputString = "";
+
+            while (number != 0)
+            {
+                /*do
+                {
+                    Console.Write("Enter a positive whole number OR enter 0 to quit.");
+                    inputString = Console.ReadLine();
+
+
+                } while (!int.TryParse(inputString, out number));*/
+
+                //Condition flag
+                //Note: it MUST be reset on EACH pass of the OUTER loop
+
+                bool validFlag = false;
+
+                do
+                {
+                    Console.Write("Enter a positive whole number OR enter 0 to quit.");
+                    inputString = Console.ReadLine();
+                    //need a test to appropriately set your loop exit test
+
+                    if (int.TryParse(inputString, out number))
+                    {
+                        //set the flag to an appropriate value to work with your logic
+                        validFlag = true;
+                    }
+                    //DON'T NEED ELSE
+
+                } while (validFlag == false);
+
+                if (number > 0)
+                {
+                    if(number % 2 == 0)
+                    {
+                        Console.WriteLine($"{number} is an even value.\n\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number} is an odd value.\n\n");
+                    }
+                }
+                else
+                {
+                    if(number == 0)
+                    {
+                        Console.WriteLine($"Thank you. Come again.\n\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number} is invalid. Try again.\n\n");
+                    }
+                }
+            }
+            //Console.WriteLine($"{validFlag} is the value of the validation flag"); IS INVALID BECAUSE validFlag DOES NOT EXIST
+            //IN THIS CODING BLOCK
+
+        }
+    }
+}
