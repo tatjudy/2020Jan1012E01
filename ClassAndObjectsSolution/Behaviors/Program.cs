fff24f838db66bab094c4d5c3f90cf6018adea50
tatjudy <59752406+tatjudy@users.noreply.github.com>
2020-03-30 18:58:20 -0600

    Objects

---
+++ ClassAndObjectsSolution/Behaviors/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behaviors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Window[] myWindows = new Window[10];
+            Window theInstance = null;
+            int logicalsize = 0;
+            try
+            {
+                theInstance = new Window(); //default constructor
+                theInstance.Model = "2 pane gas filled";
+                myWindows[logicalsize] = theInstance;
+                logicalsize++;
+
+                theInstance = new Window("single pane plexi-glass", 2.1, 1.0);
+                myWindows[logicalsize] = theInstance;
+                logicalsize++;
+
+                theInstance = new Window(); //default constructor
+                theInstance.Model = "2 pane tinted";
+                theInstance.Height = 1.75;
+                theInstance.Width = 3.2;
+                myWindows[logicalsize] = theInstance;
+                logicalsize++;
+
+                //display the instances of the array
+                for(int i = 0; i < logicalsize; i++)
+                {
+                    Console.WriteLine(myWindows[i].ToString());
+
+                    //play with the class behaviors
+
+                    Console.WriteLine("\n\nBehaviors\n");
+                    Console.WriteLine($"Area: {myWindows[i].Area()}");
+                    Console.WriteLine($"Perimeter: {myWindows[i].Perimeter()}");
+                    Console.WriteLine($"Estimate: {myWindows[i].Estimate(14.95, 2)}\n\n");
+                    
+                }
+
+                theInstance = new Window(); //bad width, looking for an exception
+                theInstance.Model = "bad width";
+                theInstance.Height = 1.75;
+                theInstance.Width = -3.2; //Error
+                Console.WriteLine(myWindows[logicalsize - 1].ToString());
+                logicalsize++;
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ ClassAndObjectsSolution/Behaviors/Window.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behaviors
+{
+    public class Window
+    {
+        private double _Width;
+        public string Model { get; set; }
+        public double Height { get; set; }
+        public double Width
+        {
+            get { return _Width; }
+            set 
+            {
+                if (value <= 0.0)
+                {
+                    throw new Exception("Your must have a positive number for your width.");
+                }
+                else
+                {
+                    _Width = value;
+                }
+            }
+        }
+        public Window()
+        {
+            Height = 1.25;
+            Width = 1.25;
+        }
+
+        public Window(string model, double height, double width)
+        {
+            Model = model;
+            Height = height;
+            //best practice for assigning values is to go through the property
+            Width = width;
+        }
+
+        //Behaviors(a.k.a. methods)
+
+        public double Area()
+        {
+            return Height * Width;
+        }
+
+        public double Perimeter()
+        {
+            return (Height + Width) * 2;
+        }
+
+        public double Estimate(double areaunitprice, int areaunit)
+        {
+            double area = Area(); //calling an internal behavior
+            return (area / areaunit) * areaunitprice;
+        }
+
+        public override string ToString()
+        {
+            return $"Model: {Model} Height: {Height}    Width: {Width}";
+        }
+    }
+
+}
