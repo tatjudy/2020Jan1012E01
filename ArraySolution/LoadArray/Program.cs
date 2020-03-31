2c60523ab14b3dfbb2d0770ac181399baac6d969
tatjudy <59752406+tatjudy@users.noreply.github.com>
2020-03-30 23:47:48 -0600

    update

--- ArraySolution/LoadArray/Program.cs
+++ ArraySolution/LoadArray/Program.cs
@@ -10,40 +10,40 @@ namespace LoadArray
     {
         static void Main(string[] args)
         {
-            //Arrays are a fixed size collection of a single data type
-            //Array names hold an internal address to the actual physical memory location of the array storage area
-            //A single item in the array is called the array element
-            //Declaring an array requires knowing the 
-            //  a)physical size
-            //  b)the elements to initialize the array
 
-            //a) array declared by physical size
+            //arrays are a fixed sixe collection of a single datatype
+            //array names hold an internal address to the actual physical
+            //   memory location of the array storage area
+            //a single item in the array is called the array element
+
+            //declaring an array requires knowing the
+            //  a) physical size
+            //  b) the elements to initialize the array
+
+            //a) array declared by phyiscal size
             int[] physicallyDeclared = new int[7];
 
             //b) the elements to initialize the array
-            //The number of values specifies the physical array size
-            //each supplied value is placed within the array consecutively
-            //the datatype after the new is optional BUT the value given MUST match the recieving array declaration type
-            int[] diceValue = new int[] { 1, 2, 3, 4, 5, 6 };
+            //   the number of values specifies the phyiscal array size
+            //   each supplied value is place within the array consecutively
+            //   the datatype after the new is optional BUT the values given
+            //       MUST match the receiving array declaration datatype.
+            int[] diceValue = new[] { 11, 2, 333, 44, 5, 56 };
 
-            //The physically declared array has an initial default value for each element depending on the system default value of the
-            //  array datatype
-            int pDSize = 7;
-
+            //the physically declared array has an initial default
+            //   value for each element depending on the system default 
+            //   value of the array datatype
+            int pDSize = physicallyDeclared.Length;
             for (int index = 0; index < pDSize; index++)
             {
                 Console.WriteLine($"physicallyDeclared array element {index + 1} located at index {index} has a value of {physicallyDeclared[index]}");
             }
-
             int dVSize = diceValue.Length;
-
             for (int index = 0; index < dVSize; index++)
             {
                 Console.WriteLine($"diceValue array element {index + 1} located at index {index} has a value of {diceValue[index]}");
             }
-
             Console.ReadKey();
-
         }
     }
 }
